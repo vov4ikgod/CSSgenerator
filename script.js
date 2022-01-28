e0946643b8fd00f33be58c76aa6695c90404d97c
vov4ik <86399820+vov4ikgod@users.noreply.github.com>
2022-01-28 22:25:58 +0500

    border-radius

--- script.js
+++ script.js
@@ -5,28 +5,53 @@ const bottomLeftRange = document.getElementById('bottom-left-range');
 const bottomRightRange = document.getElementById('bottom-right-range');
 
 // Получаем результат input range 
-const topLeftResult = document.querySelector('top-left-result');
-const topRightResult = document.querySelector('top-right-result');
-const bottomLeftResult = document.querySelector('bottom-left-result');
-const bottomRightResult = document.querySelector('bottom-right-result');
+const topLeftResult = document.querySelector('.top-left-result');
+const topRightResult = document.querySelector('.top-right-result');
+const bottomLeftResult = document.querySelector('.bottom-left-result');
+const bottomRightResult = document.querySelector('.bottom-right-result');
 
 // Получаем все input range
 const inputsRange = document.querySelectorAll('.input');
+
+// Получаем кнопку
+const btnResult = document.querySelector('.btn-result');
 
 // Получаем квадрат
 const cube = document.querySelector('.cube');
 
 // Получаем результат
-const totalResult = document.getElementById('totalResult');
+const result = document.getElementById('totalResult');
 
 
 // Связываем результаты и range
 const changeRadius = () => {
-    topLeftResult.innerHTML = topLeftRange.value;
-    topRightResult.innerHTML = topRightRange.value;
-    bottomLeftResult.innerHTML = bottomLeftRange.value;
-    bottomRightResult.innerHTML = bottomRightRange.value;
+    let valueTopLeft = topLeftRange.value;
+    topLeftResult.textContent = valueTopLeft;
+    topLeftResult.style.left = (valueTopLeft / 1.14) + '%';
+
+    let valueTopRight = topRightRange.value;
+    topRightResult.textContent = valueTopRight;
+    topRightResult.style.left = (valueTopRight / 1.14) + '%';
+
+    let valueBottomLeft = bottomLeftRange.value;
+    bottomLeftResult.textContent = valueBottomLeft;
+    bottomLeftResult.style.left = (valueBottomLeft / 1.14) + '%';
+
+    let valueBottomRight = bottomRightRange.value;
+    bottomRightResult.textContent = valueBottomRight;
+    bottomRightResult.style.left = (valueBottomRight / 1.14) + '%';
+
+
+
+    cube.style.borderRadius = topLeftRange.value + 'px ' + topRightRange.value + 'px ' + bottomRightRange.value + 'px ' + bottomLeftRange.value + 'px ';
+    console.log(cube);
 };
+
+const totalResult = () => {
+    result.innerHTML = topLeftRange.value + 'px ' + topRightRange.value + 'px ' + bottomRightRange.value + 'px ' + bottomLeftRange.value + 'px;';
+};
+
+btnResult.addEventListener('click', totalResult);
 
 inputsRange.forEach((node) => {
     node.addEventListener('input', changeRadius)
